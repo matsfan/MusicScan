0cd1201f5d54e1ee1b8bbc4bd5124ff39501da3d
Mike Price <idflyfish@hotmail.com>
2024-09-09 08:22:03 -0600

    Add project files.

---
+++ MusicScan/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicScan
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MusicScan/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MusicScan
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
